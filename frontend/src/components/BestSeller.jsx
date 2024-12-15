06c881ecbd6b02cded3f65b4c43598e08d56b841
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2024-12-15 18:10:13 +0600

    update2

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -22,9 +22,12 @@ useEffect (()=>{
                 </p>
             </div>
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
- 
-
-           </div>
+            {
+                bestSeller.map((item,index)=>(
+                    <ProductItem key={index} id={item._id} name={item.name} image={item.image}  price={item.price}/>
+                ))
+              }
+           </div>          
 
         </div>
     );
